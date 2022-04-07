7ac13d2aace7a76065953305ab2da68c7d595fe1
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-07 13:57:28 -0400

    Added many different colors to the text shadow

--- main.js
+++ main.js
@@ -15,10 +15,18 @@ function handleMove(event) {
     y = y + event.target.offsetTop
   }
 
-  const xWalk = Math.round((x / width * walk) - (walk / 2))
-  const yWalk = Math.round((y / height * walk) - (walk / 2))
+  const xWalk = Math.round((x / width * walk) - (walk / 2));
+  const yWalk = Math.round((y / height * walk) - (walk / 2));
 
-  text.style.textShadow = `${xWalk}px ${yWalk}px 0 purple`
+  text.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(148,0,211,0.7),
+      ${xWalk * - 1}px ${yWalk}px 0 rgba(75,0,130,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,0,255,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,255,0,0.7),
+      ${xWalk * -2}px ${yWalk}px 0 rgba(255,255,0,0.7),
+      ${yWalk}px ${xWalk * -2}px 0 rgba(255, 127,0,0.7),
+      ${yWalk * -2}px ${xWalk}px 0 rgba(255,0,0,0.7)
+    `;
 
 }
 
